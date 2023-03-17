30cdd7bf7a343d39ed5434634b0eaa72061a89c5
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-03-17 04:08:02 -1000

    Demo Version

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 var submitButton = $("#submit");
 const div = $("#results")
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 
 submitButton.on('click', function(){
 $("#results").empty();
 var userInput = $("input").val()
 
-$.get(`https://rickandmortyapi.com/api/character/?name=${userInput}`,(data)=>{
+$.get(`https://rickandmortyapi.com/api/character/?name=${userInput}`,async (data)=>{
 //    console.log(data.results)
 
 
@@ -21,11 +21,12 @@ for (let i = 0; i < data.results.length; i++){
     let result = data.results[i]
     // console.log(result.episode)
 
-let all = result.episode.slice(0, -1)
-console.log(result.episode.slice(0, -1))
-    $.get(all, (episode)=> {
-        
-
+    let episodes =[];
+    for (let j = 0; j < result.episode.length; j++){
+    let episode = await $.get(result.episode[j]) 
+        console.log(episode)
+        episodes.push(episode.name)
+    }
     let newDiv = $(`
     <div class="card border-danger mb-3 bg-dark-subtle" style="max-width: 18rem;">
         <div class="card-body text-danger">
@@ -36,14 +37,15 @@ console.log(result.episode.slice(0, -1))
         <div class="origin" class="card-text">Origin: ${result.origin.name}</div>
         <div class="status" class="card-text">Status: ${result.status}</div>
         <div class="location" class="card-text">Location: ${result.location.name}</div>
-        <div class="episode" class="card-text">Episode: ${episode.name}</div> 
-        <button type="button" class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Popover title" data-bs-content="${episode.name}" aria-describedby="popover6159">Episodes</button>
+        
+        <a tabindex="0" class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Episode List" data-bs-content="${episodes.join('\n')}" aria-describedby="popover6159">Episodes</a>
         </div>
        </div> 
        `)
 
        const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-       const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+       const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+       
 
        newDiv.find('.card-image').on('click', function() {
         const listener = new THREE.AudioListener();
@@ -64,12 +66,10 @@ console.log(result.episode.slice(0, -1))
 
    div.append(newDiv)
  
-     }) 
-    }  
-  
-
+     } 
+    })  
   })
-})
+
 
 
  
